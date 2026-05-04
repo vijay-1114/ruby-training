ba58993a3fb1efc7bd3d8d15f2fdcaa2ecc1f8cb
Vijay sekwadiya <vijaydhakad792@gmail.com>
2026-05-04 12:44:10 +0530

    Completed Module 1 Tasks

---
+++ math_operations.rb
@@ -0,0 +1,12 @@
+a = 10
+b = 2
+
+a1 = a+b;
+a2 = a-b;
+a3 = a*b;
+a4 = a/b;
+
+puts "Adding :-  #{a1}";
+puts "Substract :- #{a2}";
+puts "Multiply :- #{a3}";
+puts "Divide :- #{a4}";
---
+++ profile.rb
@@ -0,0 +1,7 @@
+name = "Vijay Dhakad"
+age = 23
+city = "Khachord"
+
+puts "Name :- #{name}";
+puts "Age :- #{age}";
+puts "City :- #{city}";
